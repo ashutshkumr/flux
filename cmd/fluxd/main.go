3e94e373b5d2c4b046ee564d0252e223e771eb6a
Ashutosh Kumar <ashutshkumr@gmail.com>
2021-12-30 11:32:42 +0000

    enable client-server communication

--- cmd/fluxd/main.go
+++ cmd/fluxd/main.go
@@ -1,30 +1,56 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net"
 
+	"github.com/ashutshkumr/flux/pkg/api"
 	"github.com/ashutshkumr/flux/pkg/io"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+type server struct {
+	api.UnimplementedFluxServiceServer
+}
+
 func main() {
-	d, err := io.NewNetworkDevice("veth1")
+	lis, err := net.Listen("tcp", "localhost:5000")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	svr := grpc.NewServer()
+	api.RegisterFluxServiceServer(svr, &server{})
+	log.Println("Listening on localhost:5000 ...")
+	if err := svr.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s *server) SetConfig(ctx context.Context, in *api.SetConfigRequest) (*api.SetConfigResponse, error) {
+	out := &api.SetConfigResponse{}
+
+	d, err := io.NewNetworkDevice(in.Ports[0].Ifc)
+	if err != nil {
+		out.Errors = []string{err.Error()}
+		return out, nil
+	}
 	defer d.Close()
 
-	d.SendBytes([]byte{
-		0x00, 0x00, 0x00, 0x00, 0x00, 0xAA,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0xBB,
-		0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00,
-	})
+	err = d.SendBytes(in.Flows[0].Headers[0].Raw.Bytes)
+	if err != nil {
+		out.Errors = []string{err.Error()}
+		return out, nil
+	}
+
+	return out, nil
+}
+func (s *server) SetState(ctx context.Context, in *api.SetStateRequest) (*api.SetStateResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SetState not implemented")
+}
+func (s *server) GetStats(ctx context.Context, in *api.GetStatsRequest) (*api.GetStatsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetStats not implemented")
 }
